96012239de956ac0d48cc3b9e9634e3d074fa431
Yousif <lunatic.9205@icloud.com>
2026-09-22 14:55:35 +0300

    create my entries route and sync it to the page

--- routes/entries.routes.js
+++ routes/entries.routes.js
@@ -2,10 +2,16 @@ const router = require("express").Router();
 const Entry = require("../models/Entry.js");
 const isSignedIn = require("../middleware/is-signed-in.js");
 
+router.get('/my-entries', isSignedIn, async (req, res) => {
+    const myEntries = await Entry.find({ owner: req.session.user._id })
+    res.render('my-entries.ejs', { entries: myEntries })
+})
+
 router.get("/", async (req, res) => {
     const publicEntries = await Entry.find({ isPublic: true })
     res.render("all-entries.ejs", { entries: publicEntries })
 })
+
 router.get("/new", isSignedIn, (req, res) => {
     res.render('new-entry.ejs')
 })
